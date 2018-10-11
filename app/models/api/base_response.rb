06396ccdad11c66ee339f61325c407b671025fc1
smnkrt <szymon.krywult@netguru.pl>
2018-10-11 20:24:14 +0200

    add Api::BaseResponse class

---
+++ app/models/api/base_response.rb
@@ -0,0 +1,10 @@
+module Api
+  class BaseResponse
+    attr_reader :status, :body
+
+    def initialize(status, body)
+      @status = status
+      @body = body
+    end
+  end
+end
---
+++ spec/models/api/base_response_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+
+describe Api::BaseResponse do
+  subject { described_class.new(status, body) }
+  let(:status) { 200 }
+  let(:body) { '{}' }
+
+  describe '#status' do
+    it 'returns status value' do
+      expect(subject.status).to eq(status)
+    end
+  end
+
+  describe '#body' do
+    it 'returns body value' do
+      expect(subject.body).to eq(body)
+    end
+  end
+end
