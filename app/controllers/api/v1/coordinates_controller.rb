e097fd768a005c355d7d0f9410da11b78aa9fc39
smnkrt <szymon.krywult@netguru.pl>
2018-10-15 13:49:40 +0200

    move error handling from controller to a separate service

--- app/controllers/api/v1/coordinates_controller.rb
+++ app/controllers/api/v1/coordinates_controller.rb
@@ -1,37 +1,8 @@
 module Api
   module V1
     class CoordinatesController < BaseController
-      before_action :verify_address!
-
       def fetch
-        reply_with coordinates_response
-      end
-
-      private
-
-      def verify_address!
-        reply_with Api::UnprocessableResponse.new if address_present?
-      end
-
-      def address_present?
-        address_schema.(params).failure?
-      end
-
-      def address_schema
-        # TODO: address format validation
-        Dry::Validation.JSON { required(:address).filled(:str?) }
-      end
-
-      def coordinates_response
-        Api::BaseResponse.new(200, coordinates_for_address)
-      rescue CoordinatesForAddress::GatewayTimeoutError
-        Api::GatewayErrorResponse.new(504)
-      rescue CoordinatesForAddress::BadGatewayError
-        Api::GatewayErrorResponse.new(502)
-      end
-
-      def coordinates_for_address
-        CoordinatesForAddress.call(params[:address])
+        reply_with Api::V1::Coordinates::Get.call(params)
       end
     end
   end
---
+++ app/services/api/v1/coordinates/get.rb
@@ -0,0 +1,43 @@
+module Api
+  module V1
+    module Coordinates
+      class Get
+        def self.call(params)
+          new(params).call
+        end
+
+        def initialize(params)
+          @params = params
+        end
+
+        def call
+          return Api::UnprocessableResponse.new if address_missing?
+          coordinates_response
+        end
+
+        private
+
+        def address_missing?
+          address_schema.(@params).failure?
+        end
+
+        def address_schema
+          # TODO: address format validation
+          Dry::Validation.JSON { required(:address).filled(:str?) }
+        end
+
+        def coordinates_response
+          Api::BaseResponse.new(200, coordinates_for_address)
+        rescue CoordinatesForAddress::GatewayTimeoutError
+          Api::GatewayErrorResponse.new(504)
+        rescue CoordinatesForAddress::BadGatewayError
+          Api::GatewayErrorResponse.new(502)
+        end
+
+        def coordinates_for_address
+          CoordinatesForAddress.call(@params[:address])
+        end
+      end
+    end
+  end
+end
